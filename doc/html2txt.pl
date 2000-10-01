0f0ed9c2eff72b0b8ab502288b34e49bdc534aad
tangent <tangent@noemail.net>
2000-10-01 22:20:16 +0000

    Version 1.7.6a
    FossilOrigin-Name: 71ffe79871b7c22595aeffa9759c6f46eb1956520f38d183b8b78e5688dc78cb

--- doc/html2txt.pl
+++
@@ -1,7 +0,0 @@
-opendir DIR, "man-html";
-
-foreach (readdir(DIR)) {
-  next unless /(.+?)\.html$/;
-  print "lynx -dump man-html/$_ > man-text/$1.txt\n" ;
-  system "lynx -dump man-html/$_ > man-text/$1.txt" ;
-}
